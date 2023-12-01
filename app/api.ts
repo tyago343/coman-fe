de28fc328847555d6ee680dd4a529ab8361f1b56
Santiago Casanova <tyagocasanova@gmail.com>
2023-12-01 12:42:48 +0100

    chore: adding book creation form

--- app/api.ts
+++ app/api.ts
@@ -1,3 +1,3 @@
 export function getBaseURL() {
-  return "http://localhost:3000/api/";
+  return "http://localhost:3000/";
 }
--- app/routes/book.create.tsx
+++ app/routes/book.create.tsx
@@ -1,43 +1,60 @@
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
 import { Form } from "@remix-run/react";
-
+import { getBaseURL } from "../api";
+export async function action({
+  request,
+}: ActionFunctionArgs) {
+  debugger
+  const formData = await request.formData();
+  const body = Object.fromEntries(formData.entries());
+  const newBook = await fetch(`${getBaseURL()}book`, {
+    method: "POST", // or 'PUT'
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await newBook.json();
+  return redirect(`/book/${data.id}`);
+}
 export default function BookCreate() {
   return (
     <div>
       <h1>Create a new book</h1>
-      <Form action="post">
+      <Form method="post">
         <label htmlFor="title" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none">Title</label>
         <input
           type="text"
           name="title"
           className="px-5 border-black border-[1px] border-solid rounded-sm duration-[250ms] focus:outline-none focus:border-t-[rgba(0,0,0,0.1)]"
         />
-        <label htmlFor="sinopsys" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none peer-focus:-translate-y-20">sinopsys</label>
+        <label htmlFor="synopsis" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none peer-focus:-translate-y-20">synopsis</label>
         <input
           type="text"
-          name="sinopsys"
+          name="synopsis"
           className="px-5 border-black border-[1px] border-solid rounded-sm duration-[250ms] focus:outline-none focus:border-t-[rgba(0,0,0,0.1)]"
         />
-        <label htmlFor="releaseDate" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none">Title</label>
+        <label htmlFor="publishedDate" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none">release date</label>
         <input
           type="text"
-          name="releaseDate"
+          name="publishedDate"
           className="px-5 border-black border-[1px] border-solid rounded-sm duration-[250ms] focus:outline-none focus:border-t-[rgba(0,0,0,0.1)]"
         />
-        <label htmlFor="price" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none">Title</label>
+        <label htmlFor="price" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none">price</label>
         <input
           type="text"
           name="price"
           className="px-5 border-black border-[1px] border-solid rounded-sm duration-[250ms] focus:outline-none focus:border-t-[rgba(0,0,0,0.1)]"
         />
-        <label htmlFor="author" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none">Title</label>
+        <label htmlFor="author" className="text-base block text-gray-300 translate[1.25rem, -2,5rem] duration-500 pointer-events-none">author</label>
         <input
           type="text"
           name="author"
           className="px-5 border-black border-[1px] border-solid rounded-sm duration-[250ms] focus:outline-none focus:border-t-[rgba(0,0,0,0.1)]"
         />
         <div>
-          <button type="submit">Create</button>
           <button>Cancel</button>
+          <button type="submit">Create</button>
         </div>
       </Form>
     </div>
